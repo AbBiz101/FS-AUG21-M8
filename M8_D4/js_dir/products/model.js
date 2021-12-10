7b018937a1fa58ff053f30c4e209b0d52da856a4
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-12-10 10:23:22 +0100

    push

--- M8_D4/js_dir/products/model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductModel = void 0;
-var schema_1 = require("./schema");
-var mongoose_1 = __importDefault(require("mongoose"));
-exports.ProductModel = mongoose_1.default.model('products', schema_1.ProductSchema);
